5cdb2747284dae554f3c423e61164c44fde542f7
KristianOlav <kristianolavv@gmail.com>
2017-01-26 14:42:16 +0100

    yolo-endringer

--- hello.go
+++ hello.go
@@ -2,16 +2,13 @@ package main
 
 import "fmt"
 
-<<<<<<< HEAD
+
  func main() {
    fmt.Println("Hello")
-+	fmt.Println("bye forever")
-+ fmt.Println("Whoops, I'm back!")
- }
-=======
-func main() {
+fmt.Println("bye forever")
+fmt.Println("Whoops, I'm back!")
+
 	fmt.Println("Hello")
 	fmt.Println("Is it me you're looking for?")
 	fmt.Println("bye forever")
 }
->>>>>>> origin/master
