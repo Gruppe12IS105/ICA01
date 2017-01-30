f7bfb066c0ff7228fe4cc14c15ab182390e6c465
VasuraInDrag <indrag16@student.uia.no>
2017-01-30 15:09:40 +0100

    example test2

--- hello.go
+++ hello.go
@@ -11,5 +11,5 @@ fmt.Println("Whoops, I'm back!")
 	fmt.Println("Hello")
 	fmt.Println("Is it me you're looking for?")
 	fmt.Println("bye forever")
-	fmt.Println("Forever and ever")
+	fmt.Println("Forever and ever2")
 }
