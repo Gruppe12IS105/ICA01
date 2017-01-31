5ed4c08f172c9d405c26b1682b3a15f5976cc9eb
emilr15 <emilr15@uia.no>
2017-01-31 20:40:10 +0100

    logcli er ferdig
    dvs oppg 5 er ferdig, 4 og 6 er still in progress

--- log/logbcli.go
+++ log/logbcli.go
@@ -1,14 +1,11 @@
-package main
+package functions
 
-import (
-  "fmt"
-  "math"
-  )
+import "math"
 
-func Log2(number float64) float64{
-    return math.Log(number)/math.Log(2)
-  }
+func Log2(number float64) float64 {
+	return math.Log(number) / math.Log(2)
+}
 
-  func Logb(number float64, base float64) float64{
-    return  math.Log(number)/math.Log(base)
-  }
+func Logb(number float64, base float64) float64 {
+	return math.Log(number) / math.Log(base)
+}
