f9353ff199d10ca9dfe40bc67b42955c848615be
emilr15 <emilr15@uia.no>
2017-05-12 13:01:24 +0200

    Ferdig

---
+++ logcli.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+func main() {
+	tall := os.Args[1]
+	tallFloat, _ := strconv.ParseFloat(tall, 64)
+	fmt.Println("Logaritmen av ", tall, " blir ", math.Log2(tallFloat), " med base 2")
+}
